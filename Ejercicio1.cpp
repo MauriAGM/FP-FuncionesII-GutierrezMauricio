459642d90ebc6b56fdc4e70c4f6b32e9c5e8cd06
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-10 23:03:53 -0500

    Actualizacion ejercicio 1: mejora en la interfaz, agrega validaciones, agrega bucles repetitivos y cambio de variable PI

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -5,12 +5,24 @@
 using namespace std;
 
 int main(){
+    char opcion;
     double radio, area;
-    const double PI = 3.1415;
-    
-    cout << "Ingrese el radio del circulo: ";
-    cin >> radio;
-    area = PI * pow(radio, 2);
-    cout << "El area del circulo es: " << area << endl;
+    M_PI; // Constante de pi 
+
+    do {
+        cout << "==== CALCULO DEL AREA DE UN CIRCULO =====" << endl;
+        cout << "=========================================" << endl;
+        cout << "Ingrese el radio del circulo: ";
+        cin >> radio;
+        area = M_PI * pow(radio, 2);
+        cout << "El area del circulo es: " << area << endl;
+
+        cout << "Desea calcular el area de otro circulo? (S/N): ";
+        cin >> opcion;
+        while (opcion != 'S' && opcion != 's' && opcion != 'N' && opcion != 'n') {
+            cout << "Opcion invalida. Ingrese S para si o N para no: ";
+            cin >> opcion;
+        }
+    } while (opcion == 'S' || opcion == 's');
     return 0;
 }
