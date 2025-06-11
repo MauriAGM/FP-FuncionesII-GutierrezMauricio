55034b5202314614ad1cd690044e02608970d5c4
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-10 23:16:00 -0500

    Actualizacion ejercicio 1: mejora en la interfaz y agregar validaciones al radio

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,5 +1,4 @@
 //Cálculo del Área de un Círculo
-// p * r^2
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -12,17 +11,36 @@ int main(){
     do {
         cout << "==== CALCULO DEL AREA DE UN CIRCULO =====" << endl;
         cout << "=========================================" << endl;
+        cout << endl;
         cout << "Ingrese el radio del circulo: ";
         cin >> radio;
+
+        if (radio < 0) {
+            cout << "El radio no puede ser negativo. Intente de nuevo." << endl;
+            continue;
+        }
+
         area = M_PI * pow(radio, 2);
         cout << "El area del circulo es: " << area << endl;
+        cout << endl;
 
         cout << "Desea calcular el area de otro circulo? (S/N): ";
         cin >> opcion;
+
         while (opcion != 'S' && opcion != 's' && opcion != 'N' && opcion != 'n') {
             cout << "Opcion invalida. Ingrese S para si o N para no: ";
             cin >> opcion;
         }
+        
+        if (opcion == 'N' || opcion == 'n') {
+            cout << "Gracias por usar el programa. "<< endl;
+            cout << "Finalizando programa..." << endl;
+            cout << endl;
+        } else {
+            cout << "Reiniciando el programa..." << endl;
+            cout << endl;
+        } 
+        
     } while (opcion == 'S' || opcion == 's');
     return 0;
 }
