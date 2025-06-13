b68eda90048417b23f452a96e6c44933296a75ce
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-12 23:42:36 -0500

    Version final ejercicio3 : mejora de interfaz, agregar validaciones y bucles de repeticion

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -6,13 +6,45 @@ using namespace std;
 
 int main(){
     double cateto1, cateto2, hipotenusa;
-    cout << "Ingrese el valor del primer cateto: ";
-    cin >> cateto1;
-    cout << "Ingrese el valor del segundo cateto:";
-    cin >> cateto2;
+    char opcion;
 
-    hipotenusa = sqrt(pow(cateto1, 2) + pow (cateto2, 2));
-    cout << "El valor de la hipotenusa es: " << hipotenusa << endl;
+    do {
+        cout << "====== CALCULO DE LA HIPOTENUSA ======" << endl;
+        cout << "======================================" << endl;
+        cout << endl;
 
+        cout << "Ingrese el valor del primer cateto: ";
+        cin >> cateto1;
+        if ( cateto1 <= 0 ) {
+            cout << "El cateto no puede ser negativo. Intente de nuevo." << endl;
+            return 1;
+        }
+        cout << "Ingrese el valor del segundo cateto:";
+        cin >> cateto2;
+        if ( cateto2 <= 0 ) {
+            cout << "El cateto no puede ser negativo. Intente de nuevo." << endl;
+         return 1;
+        }
+
+        hipotenusa = sqrt(pow(cateto1, 2) + pow (cateto2, 2));
+        cout << "El valor de la hipotenusa es: " << hipotenusa << endl;
+        cout << endl;
+        cout << "Desea calcular la hipotenusa de otro triangulo? (S/N): ";
+        cin >> opcion;
+                
+        while (opcion != 'S' && opcion != 's' && opcion != 'N' && opcion != 'n') {
+            cout << "Opcion invalida. Ingrese S para si o N para no: ";
+            cin >> opcion;
+        }
+        
+        if (opcion == 'N' || opcion == 'n') {
+            cout << "Gracias por usar el programa. "<< endl;
+            cout << "Finalizando programa..." << endl;
+            cout << endl;
+        } else {
+            cout << "Reiniciando el programa..." << endl;
+            cout << endl;
+        }
+    } while (opcion == 'S' || opcion == 's');
+    return 0;
 }
-
