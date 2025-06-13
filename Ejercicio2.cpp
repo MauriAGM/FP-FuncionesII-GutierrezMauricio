b5d22aa5bd7b408671365567ae014662710a34fa
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-12 23:14:33 -0500

    Version final ejercicio2 : mejora de interfaz y correccion de codigo

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -7,14 +7,9 @@ using namespace std;
 
 int main() {
     int n, cantN;
-    cout << "Ingrese el numero maximo para el generador de numeros aleatorios: ";
-    cin >> n;
-    if (n <= 0) {
-        cout << "El numero debe ser mayor que cero." << endl;
-        return 1;
-    }
-    srand(time(0));
-    int numeroALeatorio = rand() % n + 1;
+    cout << "================= GENERADOR DE NUMEROS ALEATORIOS =================" << endl;
+    cout << "===================================================================" << endl;
+    cout << endl;
 
     cout << "Ingrese la cantidad de numeros aleatorios a generar: ";
     cin >> cantN;
@@ -22,4 +17,19 @@ int main() {
         cout << "La cantida de numeros debe ser mayor a cero " << endl;
         return 1;
     }
+
+    cout << "Ingrese el numero maximo para el generador de numeros aleatorios: ";
+    cin >> n;
+    if (n <= 0) {
+        cout << "El numero debe ser mayor que cero." << endl;
+        return 1;
+    }
+    srand(time(0));
+    cout << "Generando " << cantN << " numeros aleatorios entre 1 y " << n << ":" << endl;
+    for (int i = 0; i < cantN; i++) {
+        int numeroALeatorio = rand() % n + 1;
+        cout << numeroALeatorio << " ";
+        cout << endl;
+    }
+    return 0;
 }
