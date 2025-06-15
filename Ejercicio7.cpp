5586079b31276f311c8ba4889861a0b9f17997a2
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-15 18:06:25 -0500

    Version final del ejercicio 7: Mejora en la interfaz con un mejor formato

--- Ejercicio7.cpp
+++ Ejercicio7.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int lanzarDado() {
     return rand() % 6 + 1;
 }
-void mostrarRonda( int dado1, int dado2, int puntaje1, int puntaje2) {
+void mostrarRonda(int ronda, int dado1, int dado2, int puntaje1, int puntaje2) {
+    cout << "========================================================" << endl;
+    cout << "\n============= Ronda " << ronda << " ==============" << endl;
     cout << "Jugador 1 lanzo: " << dado1 << endl;
     cout << "Jugador 2 lanzo: " << dado2 << endl;
     if (dado1 > dado2) {
@@ -16,10 +18,12 @@ void mostrarRonda( int dado1, int dado2, int puntaje1, int puntaje2) {
     } else {
         cout << "Empate en la ronda." << endl;
     }
+    cout << "Marcador: Jugador 1: " << puntaje1 << " - Jugador 2: " << puntaje2 << endl;
+    cout << endl;
 }
 int main() {
     srand(time(0)); 
-    int puntosJugador1 = 0, puntosJugador2 = 0;
+    int puntosJugador1 = 0, puntosJugador2 = 0, ronda = 1;
     while (puntosJugador1 < 3 && puntosJugador2 < 3) {
         int dado1 = lanzarDado();
         int dado2 = lanzarDado();
@@ -28,12 +32,18 @@ int main() {
         } else if (dado2 > dado1) {
             puntosJugador2++;
         }
-        mostrarRonda(dado1, dado2, puntosJugador1, puntosJugador2);
+        mostrarRonda(ronda, dado1, dado2, puntosJugador1, puntosJugador2);
+        ronda++;
     }
+
+    cout << "========================================================" << endl;
+    cout << "\n========= Resultado Final ==========" << endl;
+    cout << "Jugador 1: " << puntosJugador1 << " puntos" << endl;
+    cout << "Jugador 2: " << puntosJugador2 << " puntos" << endl;
     if (puntosJugador1 == 3) {
-        cout << "Jugador 1 ha ganado el juego!" << endl;
+        cout << "EL JUGADOR 1 HA GANADO EL JUEGO" << endl;
     } else {
-        cout << "Jugador 2 ha ganado el juego!" << endl;
+        cout << "EL JUGADOR 2 HA GANADO EL JUEGO" << endl;
     }
     return 0;
 }
